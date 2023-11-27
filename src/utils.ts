6db3234658f9218a3cd2a3c1fe9bcbdb3b530f84
kkopitehong <kkopite.hong@smartcorelink.com>
2023-11-27 17:29:51 +0800

    chore: lint

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
-import dayjs from 'dayjs';
+import dayjs from 'dayjs'
 
-export function getCurrentTime(format: string = 'YYYY MM DD HH:mm:ss'){
+export function getCurrentTime(format: string = 'YYYY MM DD HH:mm:ss') {
   return dayjs().format(format)
-  
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
   ],
   noExternal: [
     'croner',
-    'dayjs'
+    'dayjs',
   ],
 })
